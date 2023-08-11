214812acc284e8df6a32b4f3b285dae3288b3cf4
BACHKAT97 <61392719+BACHKAT97@users.noreply.github.com>
2023-08-11 21:33:19 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers of base 16 in lowercase, separated by commas and spaces.
+ * main - Prints numbers of base 16 in lowercase using 3 putchar calls.
  *
  * Return: Always 0 (Success).
  */
@@ -9,14 +9,10 @@ int main(void)
 {
 	int num;
 
-	for (num = 48; num <= 57; num++) /* ASCII for '0' to '9' */
+	for (num = 48; num <= 102; num++)
 	{
-		putchar(num);
-		if (num != 57) /* Avoid comma and space after '9' */
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		if ((num >= 48 && num <= 57) || (num >= 97 && num <= 102))
+			putchar(num);
 	}
 	putchar('\n');
 
