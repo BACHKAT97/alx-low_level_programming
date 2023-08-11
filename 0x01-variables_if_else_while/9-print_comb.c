868e385e1c442f4f99ded48e000da90b03f8ff99
BACHKAT97 <61392719+BACHKAT97@users.noreply.github.com>
2023-08-11 21:30:29 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all numbers of base 16 in lowercase, separated by commas and spaces.
+ * main - Prints numbers of base 16 in lowercase, separated by commas and spaces.
  *
  * Return: Always 0 (Success).
  */
@@ -12,7 +12,7 @@ int main(void)
 	for (num = 48; num <= 57; num++) /* ASCII for '0' to '9' */
 	{
 		putchar(num);
-		if (num != 57) /* Don't print comma and space after '9' */
+		if (num != 57) /* Avoid comma and space after '9' */
 		{
 			putchar(',');
 			putchar(' ');
