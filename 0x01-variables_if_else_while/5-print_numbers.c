55d0eae633649c6b3fc9b5389dc78b35c0eb0a30
BACHKAT97 <61392719+BACHKAT97@users.noreply.github.com>
2023-08-11 21:24:03 +0100

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 from 0.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		putchar(num + '0');
+	putchar('\n');
+
+	return (0);
+}
